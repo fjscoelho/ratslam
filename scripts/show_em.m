ea7b6d67c7f5ebf1258f49584e2f7de1706f76ba
fjscoelho <fabiojuniocoelho@gmail.com>
2025-11-18 22:00:45 -0300

    Vt_match_th = .027 for BBC_Pier

--- scripts/show_em.m
+++ scripts/show_em.m
@@ -5,10 +5,10 @@ close all;
 clc;
 
 % Set to true to save a experience map evolution video
-save_video = false;
+save_video = true;
 
 % Set to true to save partial figures of map evolution
-save_figures = false;
+save_figures = true;
 
 if save_video
     numFrames = 50; % Número de iterações/timesteps
